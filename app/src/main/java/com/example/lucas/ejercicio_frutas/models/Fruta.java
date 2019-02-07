3fe218bbe83cce120996fc2251fd302722fbe5b3
Lucas <lucasjavierespinosax@gmail.com>
2019-02-07 10:19:49 -0300

    Initial commit

---
+++ app/src/main/java/com/example/lucas/ejercicio_frutas/models/Fruta.java
@@ -0,0 +1,41 @@
+package com.example.lucas.ejercicio_frutas.models;
+
+/**
+ * Created by Lucas on 07-feb-19.
+ */
+
+public class Fruta {
+    private String nombre;
+    private String origen;
+    private String icono;
+
+    public Fruta(String nombre, String origen, String icono) {
+        this.nombre = nombre;
+        this.origen = origen;
+        this.icono = icono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getIcono() {
+        return icono;
+    }
+
+    public void setIcono(String icono) {
+        this.icono = icono;
+    }
+}
